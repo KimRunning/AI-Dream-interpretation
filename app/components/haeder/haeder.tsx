513a05afe1f4540daf540f5a34ff715337212a11
cordplace <msk7316@gmail.com>
2024-03-12 23:51:15 +0900

    style:메인페이지, 헤더 반응형 디자인 적용

--- app/components/haeder/haeder.tsx
+++ app/components/haeder/haeder.tsx
@@ -3,17 +3,27 @@ import React from "react";
 export default function Header() {
   return (
     <>
-      <section className="h-[8vh] flex flex-row items-end  w-[100%] pt-8 justify-between ">
-        <div className="w-[45vw]">
-          <div className="  pl-10 text-[2.5vw] font-bold text-transparent bg-gradient-to-r bg-clip-text from-pink via-lightBlue to-lightGreen">
-            해몽 AI - 꿈<span className="text-[2vw]">을 통해 </span> 알<span className="text-[2vw]">아보는</span> 무<span className="text-[2vw]">의식</span>
-          </div>
+      <section className="h-[36px] flex flex-row pl-1 lg:items-end lg:h-[8vh] lg:pt-8 w-[100%] justify-between ">
+        <div className="w-[170px] lg:w-[50vw] ">
+          <p className="text-[22px] flex flex-row  lg:pl-10  lg:text-[30px] 2xl:text-[46px] font-bold text-transparent bg-gradient-to-r bg-clip-text from-pink via-lightBlue to-lightGreen ">
+            해몽 AI
+            <span className="hidden lg:inline">
+              - 꿈<span className="2xl:text-[30px] lg:text-[24px]">을 통해 </span> 알<span className="2xl:text-[30px] lg:text-[24px]">아보는</span> 무
+              <span className="2xl:text-[30px] lg:text-[24px]">의식</span>
+            </span>
+          </p>
         </div>
-        <div className="flex flex-row  pr-10 pb-[5px] w-[40vw] justify-between">
-          <span className="font-bold text-[1.4vw] text-[#D5C6ED]">해몽하기</span>
-          <span className="font-bold text-[1.4vw] text-[#D5C6ED]">다른 사람 꿈 보기</span>
-          <span className="font-bold text-[1.4vw] text-[#D5C6ED]">내 꿈 보기</span>
-          <span className="font-bold text-[1.4vw] text-[#D5C6ED]">문의/제작자 정보</span>
+        <div className="inline lg:hidden flex flex-row items-end justify-between pr-2  lg:pr-10 pb-[5px] w-[390px] lg:w-[40vw] 2xl:w-[45vw]">
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[18px] text-[#D5C6ED]">해몽하기</span>
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[18px] text-[#D5C6ED]">꿈 게시판</span>
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[18px] text-[#D5C6ED]">MY</span>
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[16px] text-[#D5C6ED]">문의</span>
+        </div>
+        <div className="hidden lg:inline bg-white flex flex-row items-end justify-between pr-2  lg:pr-10 pb-[5px] w-[390px] lg:w-[40vw] 2xl:w-[45vw]">
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[18px] text-[#D5C6ED]">꿈 해몽하기</span>
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[18px] text-[#D5C6ED]">다른 사람 꿈 보러가기</span>
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[18px] text-[#D5C6ED]">내 꿈 보기</span>
+          <span className="text-[14px] font-bold 2xl:text-[22px] xl:text-[16px] text-[#D5C6ED]">제작자 / 문의</span>
         </div>
       </section>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,22 @@ export default function Chat() {
   return (
     <>
       <main className=" h-[88vh] w-[100vw] flex flex-col">
-        <section className="flex flex-col justify-around items-center w-[200px] h-[230px] mt-10 mb-1 text-[46px] text-[#F8E7E7] font-bold	mx-auto">
-          <div className="w-[194px] h-[40px]">
-            &nbsp; 꿈<span className="text-[30px]">을 통해</span>
+        <section className="flex flex-col justify-around items-center w-[200px] h-[230px] mt-5 text-[30px] pl-[40px] sm:pl-[0px] sm:text-[46px] text-[#F8E7E7] font-bold	mx-auto">
+          <div className=" w-[150px] h-[30px] sm:w-[194px] sm:h-[40px]">
+            &nbsp; 꿈<span className="text-[20px] sm:text-[30px]">을 통해</span>
           </div>
-          <div className="w-[194px] h-[40px] ">
-            &nbsp; 알<span className="text-[30px]">아보는</span>
+          <div className="w-[150px] h-[30px] sm:w-[194px] sm:h-[40px] ">
+            &nbsp; 알<span className="text-[20px] sm:text-[30px]">아보는</span>
           </div>
-          <span className="w-[194px]">
-            &nbsp; 무<span className="text-[30px]">의식</span>
+          <span className="w-[150px] h-[30px] sm:w-[194px] sm:h-[40px]">
+            &nbsp; 무<span className="text-[20px] sm:text-[30px]">의식</span>
           </span>
         </section>
-        <section className="bg-black-500 w-[40%] h-[400px] mx-auto items-center flex flex-col justify-center">
-          <span className="w-[450px] text-[20px] text-[#CBD0D9] font-semibold">당신의 꿈을 해몽 전문 AI가 분석해드려요!</span>
-          <form className="m-1 w-[460px] flex flex-col items-center justify-center" onSubmit={onSubmitHandler}>
+        <section className="w-[40%] h-[400px] mx-auto items-center flex flex-col justify-center">
+          <span className="w-[280px] sm:w-[450px] text-[20px] text-[#CBD0D9] font-semibold">해몽 전문 AI가 분석해드려요!</span>
+          <form className="w-[290px] m-1 sm:w-[460px] flex flex-col items-center justify-center" onSubmit={onSubmitHandler}>
             <textarea
-              className="w-[450px] h-[220px] bg-white bg-opacity-90"
+              className="w-[280px] h-[160px] sm:w-[450px] sm:h-[220px] bg-white bg-opacity-90"
               value={input}
               placeholder=" 당신이 꾼 꿈을 적어주세요!"
               onChange={handleInputChange}
