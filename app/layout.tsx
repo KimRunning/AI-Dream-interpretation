a80ed8f15a40e2ded31d123c7058c171a69b0b4b
cordplace <msk7316@gmail.com>
2024-05-21 19:28:55 +0900

    style: 배너 주석 처리

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,9 +56,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             {/* <div className="bg-white fixed left-2 top-[110px] z-10 w-[9%] h-[600px] hidden sm:inline">광고 사이드 배너</div> */}
             {/* <div className="bg-white fixed right-2 top-[110px] z-10 w-[9%] h-[600px] hidden sm:inline">광고 사이드 배너</div> */}
             {children}
-            <div className="flex items-center justify-center">
+            {/* <div className="flex items-center justify-center">
               <div className="fixed w-full h-[100px] bg-white sm:w-[500px] md:w-[700px] hide-on-small-height">광고배너</div>
-            </div>
+            </div> */}
           </div>
         </SearchProvider>
       </body>
