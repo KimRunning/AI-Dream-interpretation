3c3d187e820d2ec676108933e88255150bd6df09
cordplace <msk7316@gmail.com>
2024-05-21 16:21:29 +0900

    feat: 배너 광고 주석 적용, vercel.json 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,11 +53,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Header />
           <div className={"notice"}>
             <Stars />
-            <div className="bg-white fixed left-2 top-[110px] z-10 w-[9%] h-[600px] hidden sm:inline">광고 사이드 배너</div>
-            <div className="bg-white fixed right-2 top-[110px] z-10 w-[9%] h-[600px] hidden sm:inline">광고 사이드 배너</div>
+            {/* <div className="bg-white fixed left-2 top-[110px] z-10 w-[9%] h-[600px] hidden sm:inline">광고 사이드 배너</div> */}
+            {/* <div className="bg-white fixed right-2 top-[110px] z-10 w-[9%] h-[600px] hidden sm:inline">광고 사이드 배너</div> */}
             {children}
             <div className="flex items-center justify-center">
-              <div className="fixed w-full h-[100px] bg-violet-600 sm:w-[500px] md:w-[700px] hide-on-small-height">광고배너</div>
+              {/* <div className="fixed w-full h-[100px] bg-violet-600 sm:w-[500px] md:w-[700px] hide-on-small-height">광고배너</div> */}
             </div>
           </div>
         </SearchProvider>
