64f17cb45ae0962be2262d1d0464a3d7df7209c0
cordplace <msk7316@gmail.com>
2024-05-28 11:53:05 +0900

    feat:무한루프 방지

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
         source: "/:path*", // 모든 경로를 포함
         destination: "https://www.dreaminterpretationai.com/:path*", // 새 도메인으로 리다이렉트
         permanent: true, // 301 리다이렉트를 위해 true로 설정
+        has: [
+          {
+            type: "host",
+            value: "www.dreaminterpretaion.com",
+          },
+        ],
       },
     ];
   },
