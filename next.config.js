5c7d4e0062126d703667aa8a55d2292a934fd71d
cordplace <msk7316@gmail.com>
2024-05-28 11:49:19 +0900

    feat: next.config.js에서 리다이렉트 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // 모든 경로를 포함
+        destination: "https://www.dreaminterpretationai.com/:path*", // 새 도메인으로 리다이렉트
+        permanent: true, // 301 리다이렉트를 위해 true로 설정
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
